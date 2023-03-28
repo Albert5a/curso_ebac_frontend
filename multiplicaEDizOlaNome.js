9a5c3250fc0f620073c02875974b1e9296b81a1d
Albert <albertsoares97@gmail.com>
2023-03-28 11:30:12 -0300

    entregando exercicio_ts

---
+++ multiplicaEDizOlaNome.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplicar(x, y) {
+    return x * y;
+}
+const resultado = multiplicar(5, 5);
+console.log(resultado);
+function dizOla(nome) {
+    return `Olá ${nome}`;
+}
+const mensagem = dizOla("Albert");
+console.log(mensagem);
---
+++ multiplicaEDizOlaNome.ts
@@ -0,0 +1,12 @@
+function multiplicar(x: number, y: number): number {
+    return x * y;
+}
+const resultado = multiplicar(5, 5);
+console.log(resultado);
+
+function dizOla(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+const mensagem = dizOla("Albert");
+console.log(mensagem);
